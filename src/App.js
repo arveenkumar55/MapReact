c0f7f7aa2b754c3b1da9291ba06276ba805aaef4
Papadopoulos Christos <c_papado@hotmail.com>
2023-03-23 09:33:12 +0500

    background_issues

--- src/App.js
+++ src/App.js
@@ -1,100 +1,110 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { Input, List } from "antd";
 import { countryList } from "./util";
 
-import Map from './views/Map'
+import Map from "./views/Map";
 
-function sleep(time){
-    return new Promise((resolve)=>setTimeout(resolve,time)
-  )
-  }
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
-  export default function App()  {
-
-    
-  const {CountryListnName} = useSelector(state => state.SaveSearchReducer);
+export default function App() {
+  const { CountryListnName } = useSelector((state) => state.SaveSearchReducer);
 
   //Use for all the dispatch actions
   const dispatch = useDispatch();
 
-    const [value, setValue] = useState("Malaysia");
-    const [countryLatLong, setCountryLatLong] = useState(  [
-        "Malaysia",
-        2.5,
-        112.5
-    ])
-    const [isPlacePredictionsLoading, PlacePredictionsLoading] = useState(false);
-  
-    const [countries, setCountries] = useState(countryList.filter((country)=> country[0].toLowerCase().includes('Malaysia'.toLowerCase())))
-  
-  
+  const [value, setValue] = useState("Malaysia");
+  const [countryLatLong, setCountryLatLong] = useState([
+    "Malaysia",
+    2.5,
+    112.5,
+  ]);
+  const [isPlacePredictionsLoading, PlacePredictionsLoading] = useState(false);
 
-    function setLatandLong (countryLatLon) {
-        setCountryLatLong(countryLatLon)
-        setValue(countryLatLon[0])
-        dispatch({type:'SAVE_USER_INPUT',payload:countryLatLon[0]});
-    }
-    async  function searchCountry (value) {
-  
+  const [countries, setCountries] = useState(
+    countryList.filter((country) =>
+      country[0].toLowerCase().includes("Malaysia".toLowerCase())
+    )
+  );
+
+  function setLatandLong(countryLatLon) {
+    setCountryLatLong(countryLatLon);
+    setValue(countryLatLon[0]);
+    dispatch({ type: "SAVE_USER_INPUT", payload: countryLatLon[0] });
+  }
+  async function searchCountry(value) {
     //   setCountryLatLong(value)
-      PlacePredictionsLoading(true)
-      let filterCountries = countryList.filter((country)=> country[0].toLowerCase().includes(value.toLowerCase()))
-      setCountries(filterCountries)
-      await sleep(1000);
-      PlacePredictionsLoading(false)
-    }
-  
-  
-  
-    return (
-        <div className="App">
-        <main className="container">
-    
-    <div style={{display: 'flex', marginTop:'20px'}}> 
-    <div style={{width:"50%"}}>    
-     <div style={{ width: "250px" , marginBottom: '100px' }}>
-        <span>Country</span>
-        <Input.Search
-          style={{ color: "black" }}
-          value={value}
-          placeholder="Enter Country Name"
-          onChange={(evt) => {
-            setValue(evt.target.value);
-            searchCountry(evt.target.value)
-          }}
-          loading={isPlacePredictionsLoading}
-        />
-          {!isPlacePredictionsLoading && (
-                  <List
+    PlacePredictionsLoading(true);
+    let filterCountries = countryList.filter((country) =>
+      country[0].toLowerCase().includes(value.toLowerCase())
+    );
+    setCountries(filterCountries);
+    await sleep(1000);
+    PlacePredictionsLoading(false);
+  }
+
+  return (
+    <div className="App">
+      <main className="container">
+        <div style={{ display: "flex", marginTop: "20px" }}>
+          <div style={{ width: "50%" }}>
+            <div style={{ width: "250px", marginBottom: "100px" }}>
+              <span>Country</span>
+              <Input.Search
+                style={{ color: "black" }}
+                value={value}
+                placeholder="Enter Country Name"
+                onChange={(evt) => {
+                  setValue(evt.target.value);
+                  searchCountry(evt.target.value);
+                }}
+                loading={isPlacePredictionsLoading}
+              />
+              {!isPlacePredictionsLoading && (
+                <List
                   size="small"
-                  style={{maxHeight:'200px', overflow:'auto', minWidth:'200px'}}
+                  style={{
+                    maxHeight: "200px",
+                    overflow: "auto",
+                    minWidth: "200px",
+                  }}
                   bordered
                   dataSource={countries}
-                  renderItem={(item) => <List.Item onClick={() => setLatandLong(item)} >{item[0]}</List.Item>}
+                  renderItem={(item) => (
+                    <List.Item
+                      style={{ cursor: "pointer" }}
+                      onClick={() => setLatandLong(item)}
+                    >
+                      {item[0]}
+                    </List.Item>
+                  )}
                 />
-          )}
+              )}
+            </div>
+          </div>
+          <div style={{ width: "50%" }}>
+            <div style={{ width: "250px", marginBottom: "100px" }}>
+              <span>User History Search(Redux Used)</span>
+              <List
+                size="small"
+                style={{
+                  maxHeight: "200px",
+                  overflow: "auto",
+                  minWidth: "200px",
+                }}
+                bordered
+                dataSource={CountryListnName}
+                renderItem={(item) => <List.Item>{item}</List.Item>}
+              />
+            </div>
+          </div>
         </div>
-        </div>
-        <div style={{width:"50%"}}>    
 
-        <div style={{ width: "250px" , marginBottom: '100px' }}>
-        <span>User History Search(Redux Used)</span>
-                  <List
-                  size="small"
-                  style={{maxHeight:'200px', overflow:'auto', minWidth:'200px'}}
-                  bordered
-                  dataSource={CountryListnName}
-                  renderItem={(item) => <List.Item  >{item}</List.Item>}
-                />
-        </div>
-        </div>
-        </div>
-
-
-            <Map countryLatLong={countryLatLong}/>
-        </main>
-        </div>
-    );
-  };
+        <Map countryLatLong={countryLatLong} />
+      </main>
+    </div>
+  );
+}
--- src/views/Map.js
+++ src/views/Map.js
@@ -1,75 +1,43 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 
-
-export default function SimpleMap({countryLatLong}){
-
-  console.log('countryLatLong', countryLatLong)
+export default function SimpleMap({ countryLatLong }) {
   const defaultProps = {
-    center: {lat: 2.5, lng:  112.5},
-    zoom: 11
+    center: { lat: 2.5, lng: 112.5 },
+    zoom: 11,
   };
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-          <GoogleMapReact
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-        >
-          <AnyReactComponent 
-            lat={parseFloat(countryLatLong[1])} 
-            lng={parseFloat(countryLatLong[2])} 
-            text={countryLatLong[0]} 
-          />
-        </GoogleMapReact>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={parseFloat(countryLatLong[1])}
+          lng={parseFloat(countryLatLong[2])}
+          text={countryLatLong[0]}
+        />
+      </GoogleMapReact>
     </div>
   );
 }
 
-
 const AnyReactComponent = ({ text }) => (
-    <div style={{
-      color: 'white', 
-      background: 'grey',
-      padding: '15px 10px',
-      display: 'inline-flex',
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: '100%',
-      transform: 'translate(-50%, -50%)'
-    }}>
-      {text}
-    </div>
-  );
-  
-//   class SimpleMap extends React.Component {
-//     static defaultProps = {
-//       center: {lat: 59.95, lng: 30.33},
-//       zoom: 11
-//     };
-  
-//     render() {
-//       return (
-//          <GoogleMapReact
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent 
-//             lat={59.955413} 
-//             lng={30.337844} 
-//             text={'Kreyser Avrora'} 
-//           />
-//         </GoogleMapReact>
-//       );
-//     }
-//   }
-  
-  
-//   ReactDOM.render(
-//     <div style={{width: '100%', height: '400px'}}>
-//       <SimpleMap/>
-//     </div>,
-//     document.getElementById('main')
-//   );
-  
+  <div
+    style={{
+      color: "white",
+      background: "grey",
+      padding: "15px 10px",
+      display: "inline-flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "100%",
+      transform: "translate(-50%, -50%)",
+    }}
+  >
+    {text}
+  </div>
+);
+
